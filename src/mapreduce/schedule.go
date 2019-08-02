aa5516e3ba92e2803af08cdcb767060dbf9e748d
qeesung <1245712564@qq.com>
2019-08-03 01:37:38 +0800

    implement the schedule task to worker

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,13 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/rpc"
+	"strconv"
+	"sync"
+	"sync/atomic"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +37,64 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+	taskChan := make(chan string)
+	go func() {
+		// Publish the task to the task channel
+		switch phase {
+		case mapPhase:
+			for _, mapFile := range mapFiles {
+				taskChan <- mapFile
+			}
+		case reducePhase:
+			for i := 0; i < nReduce; i++ {
+				taskChan <- strconv.Itoa(i)
+			}
+		}
+		close(taskChan) // all task scheduled
+	}()
+
+	var taskIndex int32
+	wg := sync.WaitGroup{}
+	wg.Add(ntasks)
+	go func() { // loop the workers
+		for workerAddress := range registerChan {
+			wa := workerAddress
+			go func() { // rpc call, schedule the task to worker
+				workerClient, err := rpc.Dial("unix", wa)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				for task := range taskChan {
+					var taskArg DoTaskArgs
+					switch phase {
+					case mapPhase:
+						taskArg = DoTaskArgs{
+							JobName:       jobName,
+							File:          task,
+							Phase:         phase,
+							TaskNumber:    int(taskIndex),
+							NumOtherPhase: n_other,
+						}
+					case reducePhase:
+						reduceNumber, _ := strconv.Atoi(task)
+						taskArg = DoTaskArgs{
+							JobName:       jobName,
+							Phase:         phase,
+							TaskNumber:    reduceNumber,
+							NumOtherPhase: n_other,
+						}
+					}
+					atomic.AddInt32(&taskIndex, 1)
+					err = workerClient.Call("Worker.DoTask", taskArg, nil)
+					if err != nil {
+						log.Fatal(err.Error())
+					}
+					wg.Done()
+				}
+			}()
+		}
+	}()
+	wg.Wait()
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
